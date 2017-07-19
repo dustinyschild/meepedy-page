b3c51a34d22caf8d82d4e8e538d8006bf204204f
Meepedy <david@psyon.org>
2017-07-19 08:40:30 -0500

    Added project-data.js with 3 entries for testing

---
+++ public/js/project-data.js
@@ -0,0 +1,20 @@
+var rawData = [
+  {
+    title: 'Mr. Runner',
+    Image: 'img/mr-runner.jpg',
+    Description: 'A simple endless runner created out of boredom late at night.',
+    Url: 'https://play.google.com/store/apps/details?id=com.boredfreak.mrrunner&hl=en',
+  },
+  {
+    title: 'Tell Me Something Funny',
+    Image: 'img/tell-me-something-funny.jpg',
+    Description: 'Feeling unamused and bored? This will tell you something funny.',
+    Url: 'https://tellmesomethingfunny.com',
+  },
+  {
+    title: 'Simle Snake',
+    Image: 'img/simple-snake.jpg',
+    Description: 'A simple snake game for those you want to enjoy a classic',
+    Url: 'https://simplesnake.com',
+  }
+]
